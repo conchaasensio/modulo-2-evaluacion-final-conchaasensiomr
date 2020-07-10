ab998b2e4f3b6c0c411e63988bda0001a709abae
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-10 19:22:25 +0200

    Add favorites array and paint favorites in handle function

--- src/js/main.js
+++ src/js/main.js
@@ -22,8 +22,32 @@ const getDataFromApi = () => {
 };
 //console.log(shows);
 
+//Favorites
+
 const handleSetFavorite = (event) => {
   event.currentTarget.classList.add('selected');
+  favorites.push(shows[event.currentTarget.dataset.index]);
+  paintFavorites();
+};
+
+const favoritesItems = document.querySelector('.js-favorites-items');
+const paintFavorites = () => {
+  let codeHTML = '';
+  for (let index = 0; index < favorites.length; index++) {
+    const favorite = favorites[index];
+    let imageUrl =
+      favorite.show.image !== null
+        ? favorite.show.image.medium
+        : 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+    codeHTML += `<li class="main__favorites--item js-favorites-items" data-index="${index}">`;
+    codeHTML += `<article>`;
+    codeHTML += `<img src="${imageUrl}" alt="" />`; //evaluar condición de que si tiene contenido y si es null, ponga la url por defecto (ternario).
+    codeHTML += `<p class="main__list--title js-favorites-name">${favorite.show.name}</p>`;
+    codeHTML += `</article>`;
+    codeHTML += `</li>`;
+  }
+  const favoritesItems = document.querySelector('.js-favorites-items');
+  favoritesItems.innerHTML = codeHTML;
 };
 
 //catalog
@@ -31,15 +55,16 @@ const handleSetFavorite = (event) => {
 const showsItems = document.querySelector('.js-shows-list');
 const paintShows = () => {
   let codeHTML = '';
-  for (const show of shows) {
+  for (let index = 0; index < shows.length; index++) {
+    const show = shows[index];
     let imageUrl =
       show.show.image !== null
         ? show.show.image.medium
         : 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-    codeHTML += `<li class="main__list--item js-shows-items">`;
+    codeHTML += `<li class="main__list--item js-shows-items" data-index="${index}">`;
     codeHTML += `<article>`;
     codeHTML += `<img src="${imageUrl}" alt="" />`; //evaluar condición de que si tiene contenido y si es null, ponga la url por defecto (ternario).
-    codeHTML += `<p class="main__list--title">${show.show.name}</p>`;
+    codeHTML += `<p class="main__list--name js-list-name">${show.show.name}</p>`;
     codeHTML += `</article>`;
     codeHTML += `</li>`;
   }
