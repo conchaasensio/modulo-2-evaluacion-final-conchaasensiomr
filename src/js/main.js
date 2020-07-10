220fa74c816d2dae138571e4ff623a41d68941c2
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-10 11:48:21 +0200

    Add paintShows function

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,23 @@
 'use strict';
 
-//api
+//Obtener los datos de la API
 
 let shows = [];
 
 const getDataFromApi = () => {
-  fetch(`http://api.tvmaze.com/search/shows?q=girls`)
+  fetch(`http://api.tvmaze.com/search/shows?q=game`)
     .then((response) => response.json())
     .then((data) => {
       for (let i = 0; i < data.length; i++) {
-        const show = data[i];
-        shows.push(show);
+        //const show = data[i];
+        shows.push(data[i]);
       }
       paintShows();
+      console.log(shows);
     });
 };
 getDataFromApi();
-console.log(shows);
+//console.log(shows);
 
 //catalog
 
@@ -25,10 +26,14 @@ const paintShows = () => {
   let codeHTML = '';
   for (const show of shows) {
     codeHTML += `<article class="main__list--item js-shows-items">`;
-    codeHTML += `<img src="${show.show.image.medium}" alt="" />`;
+    codeHTML += `<img src="${show.show.image.medium}" alt="" />`; //evaluar condición de que si tiene contenido y si es null, ponga la url por defecto (ternario).
+    // show.show.image !== null
+    //   ? img src="${show.show.image.medium}";
+    //   : img src="https://via.placeholder.com/150C/O https://placeholder.com/";
     codeHTML += `<p class="main__list--title">${show.show.name}</p>`;
     codeHTML += `</article class="main__list--item js-shows-items">`;
   }
   const showsItems = document.querySelector('.js-shows-items');
   showsItems.innerHTML = codeHTML;
+  // console.log(showsItems);
 };
