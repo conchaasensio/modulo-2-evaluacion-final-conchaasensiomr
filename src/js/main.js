33ba141dabf1399cc3c00e363865643088338fba
Concha Asensio <conchaasensiomr@gmail.com>
2020-07-10 13:29:14 +0200

    Add variable for image and set placeholder

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,18 @@
 'use strict';
 
+const showsSearchButton = document.querySelector('.js-search-button');
+
 //Obtener los datos de la API
 
 let shows = [];
 
 const getDataFromApi = () => {
-  fetch(`http://api.tvmaze.com/search/shows?q=game`)
+  fetch(`http://api.tvmaze.com/search/shows?q=girls`)
     .then((response) => response.json())
     .then((data) => {
       for (let i = 0; i < data.length; i++) {
-        //const show = data[i];
-        shows.push(data[i]);
+        const show = data[i];
+        shows.push(show);
       }
       paintShows();
       console.log(shows);
@@ -25,11 +27,13 @@ const showsItems = document.querySelector('.js-shows-items');
 const paintShows = () => {
   let codeHTML = '';
   for (const show of shows) {
+    let imageUrl =
+      show.show.image !== null
+        ? show.show.image.medium
+        : 'https://via.placeholder.com/150';
+
     codeHTML += `<article class="main__list--item js-shows-items">`;
-    codeHTML += `<img src="${show.show.image.medium}" alt="" />`; //evaluar condición de que si tiene contenido y si es null, ponga la url por defecto (ternario).
-    // show.show.image !== null
-    //   ? img src="${show.show.image.medium}";
-    //   : img src="https://via.placeholder.com/150C/O https://placeholder.com/";
+    codeHTML += `<img src="${imageUrl}" alt="" />`; //evaluar condición de que si tiene contenido y si es null, ponga la url por defecto (ternario).
     codeHTML += `<p class="main__list--title">${show.show.name}</p>`;
     codeHTML += `</article class="main__list--item js-shows-items">`;
   }
@@ -37,3 +41,13 @@ const paintShows = () => {
   showsItems.innerHTML = codeHTML;
   // console.log(showsItems);
 };
+
+getDataFromApi();
+
+// events
+
+const handleshowsSearchClick = () => {
+  console.log('me han clickado');
+};
+
+showsSearchButton.addEventListener('click', handleshowsSearchClick);
